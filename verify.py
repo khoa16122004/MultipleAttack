7b80013d94a9916b9ed756415eb445c56029537a
khoa16122004 <khoatr.work@gmail.com>
2025-04-02 11:49:52 +0700

    Update verify.py

--- verify.py
+++ verify.py
@@ -9,12 +9,13 @@ model = LLava("llava-onevision-qwen2-7b-ov", "llava_qwen")
 
 qs = "Which images feature elements typically associated with celebration or festive occasions?<image><image><image><image>"
 # kiểm định xem ảnh float inference có khác ảnh int ko
-img_files = [Image.open(os.path.join(clean_image_dir, path)).convert("RGB") for path in sorted(os.listdir(clean_image_dir))]
+img_files = [Image.open(os.path.join(clean_image_dir, f"{j}.png")).convert("RGB") for j in range(len(os.listdir(clean_image_dir)))]
 adv_img_file = Image.open(r"/data/elo/khoatn/MultipleAttack/test_ES_lambda=50_epsilon=0.01_maxiter=1_pretrained=llava-onevision-qwen2-7b-ov/30/0/adv.png")
 img_files[0] = adv_img_file
 print("Size: ", adv_img_file.size)
 img_files[0].save("testadv.png")
 input_ids, image_tensors_0, image_sizes = model.repair_input(qs, img_files)
+print("imgsize: ", image_sizes)
 tensor_output = model.inference(input_ids, image_tensors_0, image_sizes)
 print("PIL inference: ", tensor_output)
 
