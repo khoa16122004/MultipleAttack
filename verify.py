483dbe8cfce2c0b873c37baaaa8ade80d8a55d4b
khoa16122004 <khoatr.work@gmail.com>
2025-04-02 11:32:13 +0700

    Update verify.py

--- verify.py
+++ verify.py
@@ -10,13 +10,13 @@ model = LLava("llava-onevision-qwen2-7b-ov", "llava_qwen")
 qs = "Which images feature elements typically associated with celebration or festive occasions?<image><image><image><image>"
 # kiểm định xem ảnh float inference có khác ảnh int ko
 img_files = [Image.open(os.path.join(clean_image_dir, path)).convert("RGB") for path in sorted(os.listdir(clean_image_dir))]
-adv_img_file = Image.open("D:\MultipleAttack\test_ES_lambda=50_epsilon=0.01_maxiter=1_pretrained=llava-onevision-qwen2-7b-ov/0/adv.png")
+adv_img_file = Image.open(r"test_ES_lambda=50_epsilon=0.01_maxiter=1_pretrained=llava-onevision-qwen2-7b-ov/0/adv.png")
 img_files[0] = adv_img_file
 input_ids, image_tensors_0, image_sizes = model.repair_input(qs, img_files)
 tensor_output = model.inference(input_ids, image_tensors_0, image_sizes)
 print("PIL inference: ", tensor_output)
 
-image_tensor_load = torch.load("D:\MultipleAttack\test_ES_lambda=50_epsilon=0.01_maxiter=1_pretrained=llava-onevision-qwen2-7b-ov/0/adv.pt")
+image_tensor_load = torch.load(r"test_ES_lambda=50_epsilon=0.01_maxiter=1_pretrained=llava-onevision-qwen2-7b-ov/0/adv.pt")
 tensor_output = model.inference(input_ids, image_tensor_load, image_sizes)
 print("Tensor inference: ", tensor_output)
 
