b329e82120578a8acda5505ea8a5c98bce02d208
khoa16122004 <khoatr.work@gmail.com>
2025-04-02 09:24:37 +0700

    Update verify.py

--- verify.py
+++ verify.py
@@ -2,7 +2,8 @@ from models.llava_ import LLava
 from PIL import Image
 import os
 clean_image_dir = "/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.1_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/0/clean_img"
-
+output_dir = "verify"
+os.makedirs(output_dir, exist_ok=True)
 model = LLava("llava-onevision-qwen2-7b-ov", "llava_qwen")
 
 img_files = [Image.open(os.path.listdir(clean_image_dir, path)).convert("RGB") for path in sorted(os.listdir(clean_image_dir))]
@@ -10,6 +11,6 @@ input_ids, image_tensors, image_sizes = model.repair_input(None, img_files)
 
 decoded_image = model.decode_image_tensors(image_tensors)
 for i in range(len(decoded_image)):
-    decoded_image[i].save(os.path.join(clean_image_dir, f"decoded_image{i}.png"))
-    
+    decoded_image[i].save(os.path.join(output_dir, f"decoded_image{i}.png"))
+    img_files[i].save(os.path.join(output_dir, f"original_image{i}.png"))
     
