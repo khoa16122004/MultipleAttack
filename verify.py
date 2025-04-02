997bbe7308b47dccd4614d4e713db028af1b2a8d
khoa16122004 <khoatr.work@gmail.com>
2025-04-02 09:48:47 +0700

    Update verify.py

--- verify.py
+++ verify.py
@@ -10,7 +10,7 @@ model = LLava("llava-onevision-qwen2-7b-ov", "llava_qwen")
 
 # kiểm định xem ảnh float inference có khác ảnh int ko
 img_files = [Image.open(os.path.join(clean_image_dir, path)).convert("RGB").resize((224, 224)) for path in sorted(os.listdir(clean_image_dir))]
-input_ids, image_tensors, image_sizes = model.repair_input(None, img_files)
+input_ids, image_tensors, image_sizes = model.repair_input("Descibe image", img_files)
 image_tensors += torch.randn_like(image_tensors).cuda() * 0.05 # ảnh float
 tensor_output = model.inference(input_ids, image_tensors, image_sizes)
 print(tensor_output)
@@ -18,7 +18,7 @@ print(tensor_output)
 
 decoded_image_pil = model.decode_image_tensors(image_tensors) # ảnh int
 
-input_ids, image_tensors, image_sizes = model.repair_input(None, decoded_image_pil)
+input_ids, image_tensors, image_sizes = model.repair_input("Descibe image", decoded_image_pil)
 tensor_output = model.inference(input_ids, image_tensors, image_sizes)
 for i in range(len(decoded_image_pil)):
     decoded_image_pil[i].save(os.path.join(output_dir, f"decoded_image{i}.png"))
