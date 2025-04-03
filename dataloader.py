df3437f6df0177076275b2aff96870190436cd44
khoa16122004 <khoatr.work@gmail.com>
2025-04-03 14:27:18 +0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,8 +4,7 @@ from torch.utils.data import Dataset
 import datasets
 
 def mantis_QA_loader(image_placeholder):
-    dataset = datasets.load_dataset("TIGER-Lab/Mantis-Instruct", "multi_vqa", revision="script",
-                                    cache_dir=".cache")
+    dataset = datasets.load_dataset("TIGER-Lab/Mantis-Instruct", "multi_vqa", revision="script")
     
     for item in tqdm(dataset['train']):
         id = item['id']
