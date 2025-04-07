c47dba0f71cf510b78ff03985b48d0cf17578da0
khoa16122004 <khoatr.work@gmail.com>
2025-04-07 13:38:20 +0700

    Update test_whitebox.py

--- test_whitebox.py
+++ test_whitebox.py
@@ -20,7 +20,8 @@ def main(args):
                 
         # inference
         input_ids, image_tensors, image_sizes = model.repair_input(qs, img_files)
-        model.model(input_ids=input_ids, images=image_tensors, image_sizes=image_sizes)
+        output = model.model(input_ids=input_ids, images=image_tensors, image_sizes=image_sizes)
+        print(output.shape)
         break
     
      
